bfda5803759d5f82561d2b7ba1bf81252c51cfe8
AMS21 <hansfritzenmeister@yahoo.de>
2019-03-24 20:09:31 +0100

    Added test for BitWiseOrable

--- main.cpp
+++ main.cpp
@@ -168,6 +168,16 @@ TEST_CASE("BitWiseAndable")
     CHECK(s1.get() == (2 & 64));
 }
 
+TEST_CASE("BitWiseOrable")
+{
+    using BitWiseOrableType = fluent::NamedType<int, struct BitWiseOrableTag, fluent::BitWiseOrable>;
+    BitWiseOrableType s1(2);
+    BitWiseOrableType s2(64);
+    CHECK((s1 | s2).get() == (2 | 64));
+    s1 |= s2;
+    CHECK(s1.get() == (2 | 64));
+}
+
 TEST_CASE("Comparable")
 {
     REQUIRE((10_meter == 10_meter));
