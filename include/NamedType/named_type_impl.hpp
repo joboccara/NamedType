4c70e6efb7687853c4f96183ebc254a30a871229
Stanley Pinchak <stanley.pinchak@gmail.com>
2022-04-02 13:20:53 -0500

    Replace whitespace

--- include/NamedType/named_type_impl.hpp
+++ include/NamedType/named_type_impl.hpp
@@ -81,6 +81,7 @@ public:
       constexpr NamedType(in_place_t tag, Args&&... args) noexcept(std::is_nothrow_constructible<T>::value): value_(std::forward<Args>(args)...)
     {
     }
+
     FLUENT_NODISCARD constexpr std::remove_reference_t<T> const& get() const noexcept
     {
         return value_;
